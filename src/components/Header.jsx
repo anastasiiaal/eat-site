4f64354a02ed229e422a37dddbabe8af713d805b
Anastasiia A <95031679+anastasiiaal@users.noreply.github.com>
2023-09-15 16:44:28 +0200

    💄 index page carousel setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import logoImg from "/src/assets/logo.svg"
+
 export default function Header() {
     return (
         <nav className="navbar navbar-expand-sm bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div className="container-fluid">
+            <div className="container">
                 <a className="navbar-brand" href="#">
-                    <img className="logo" src="./src/assets/logo.svg" alt="Logo" />
+                    <img className="logo" src={logoImg} alt="Logo" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -14,8 +16,7 @@ export default function Header() {
                         <a className="nav-link px-4 active" aria-current="page" href="#">Accueil</a>
                         <a className="nav-link px-4" href="#">La carte</a>
                         <a className="nav-link px-4" href="#">Contact</a>
-                        <a className="nav-link px-4" href="#"
-                        >Réserver</a>
+                        <a className="nav-link px-4" href="#">Réserver</a>
                     </div>
                 </div>
             </div>
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,7 +1,55 @@
+import carouselOne from "/src/assets/carousel-1.jpg"
+import carouselTwo from "/src/assets/carousel-2.jpg"
+import carouselThree from "/src/assets/carousel-3.jpg"
+
 export default function Accueil() {
     return (
-        <div className="container">
-            <h1>accueil</h1>
-        </div>
+        <>
+            <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-bs-interval="5000">
+                        <img src={carouselOne} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h1>Estaminet, Aromes et Tentations</h1>
+                            <p className="my-4">Une cuisine traditionnelle Française et plus !</p>
+                            <button type="button" className="btn btn-warning mb-5 btn-lg">Réserver une table</button>
+                        </div>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="5000">
+                        <img src={carouselTwo} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h1>Estaminet, Aromes et Tentations</h1>
+                            <p className="my-4">Patricia & Sébastien vous souhaitent la BIENVENUE !</p>
+                            <button type="button" className="btn btn-warning mb-5 btn-lg">Réserver une table</button>
+                        </div>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="5000">
+                        <img src={carouselThree} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h1>Estaminet, Aromes et Tentations</h1>
+                            <p className="my-4">On vous attend pour partager un vrai goût provençal</p>
+                            <button type="button" className="btn btn-warning mb-5 btn-lg">Réserver une table</button>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div className="container">
+                <h2>hello</h2>
+            </div>
+        </>
     )
 }
