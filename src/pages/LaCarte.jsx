1cc5622e9ab873af3994a7fb3f28fd6c98f11b4b
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2023-09-29 14:37:30 +0200

    added menu json + LaCarte page done

--- src/pages/LaCarte.jsx
+++ src/pages/LaCarte.jsx
@@ -1,7 +1,29 @@
 import SubHeader from "../components/SubHeader"
 import subheaderImg from "/src/assets/subheader-carte.jpg"
+import menu from "../../carte.json"
 
 export default function LaCarte () {
+    const menuData = menu
+    console.log(menuData)
+
+    const starters = menuData.starters.items.map(item => {
+        return (
+            <p>{item}</p>
+        )
+    })
+
+    const main = menuData.main.items.map(item => {
+        return (
+            <p>{item}</p>
+        )
+    })
+
+    const dessert = menuData.dessert.items.map(item => {
+        return (
+            <p>{item}</p>
+        )
+    })
+
     return (
         <>
             <SubHeader
@@ -10,7 +32,20 @@ export default function LaCarte () {
                 subtitle="Une cuisine traditionnelle Française"
                 textColor="white"
             />
-            <h1>Carte ici</h1>
+            
+            <section className="craft" id="carte">
+                <div className="container">
+                    <div className="carte-wrapper text-center">
+                        <h2>Carte {menuData.mois}</h2>
+                        <h3>Entrées - {menuData.starters.prix}€</h3>
+                        {starters}
+                        <h3>Plats</h3>
+                        {main}
+                        <h3>Desserts - {menuData.dessert.prix}€</h3>
+                        {dessert}
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
